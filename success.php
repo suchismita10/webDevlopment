fee7095e017701f4a46e5d51e8c8f25768600d43
suchismita10 <73934084+suchismita10@users.noreply.github.com>
2020-12-15 20:36:55 +0530

    Add files via upload

---
+++ success.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+        <center>
+            <h1>Payment Successful</h1>
+            <h2>Thank You for the donation!!</h2>
+        </center>
+        
+            
+    </form>
+
+
+</body>
+</html>
